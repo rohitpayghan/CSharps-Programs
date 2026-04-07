3f1bf58a9dd841def0e4d12f56120cce476983b9
Rohit Payghan <156587897+rohitpayghan@users.noreply.github.com>
2026-04-07 16:31:39 +0530

    Add program to calculate area of a rectangle

---
+++ Basic/Area of rectangle/program.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        double l = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Area = " + (l * b));
+    }
+}
